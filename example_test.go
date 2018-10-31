06f3ebd06a2afb165068d2f57a55a0bd8f13f063
Ido Kanner <idok@linesip.com>
2018-10-31 17:18:34 +0200

    starting of documentation

--- example_test.go
+++ example_test.go
@@ -1,1 +1,27 @@
-package gostrutils
+package gostrutils_test
+
+import (
+	"fmt"
+
+	"github.com/ik5/gostrutils"
+)
+
+func ExampleDetectMimeTypeFromContent() {
+	hello := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
+
+	fmt.Printf("Mime type of %s is: %s", hello, gostrutils.DetectMimeTypeFromContent(hello))
+}
+
+func ExampleStrToPointer() {
+	hello := "hello"
+	addr := gostrutils.StrToPointer(hello)
+	fmt.Printf("String: %s, Address: %p\n", hello, addr)
+}
+
+func ExamplePointerToStr() {
+	empty := gostrutils.PointerToStr(nil)
+	hello := "hello"
+	pHello := &hello
+	newHello := gostrutils.PointerToStr(pHello)
+	fmt.Printf("empty: %s, pHello: %s\n", empty, newHello)
+}
