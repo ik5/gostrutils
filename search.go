70f4d0cc1ac04e19c9fbad66f915a3e9d0cdc6b9
Ido Kanner <idok@linesip.com>
2018-10-31 18:28:18 +0200

    More search functions with tests

--- search.go
+++ search.go
@@ -1,10 +1,15 @@
 package gostrutils
 
-// SearchStringInSlice looks for a string inside a slice
-func SearchStringInSlice(list []string, needle string) bool {
+// GetStringIndexInSlice returns the index of the slice if string was found or -1 if not
+func GetStringIndexInSlice(list []string, needle string) int {
 	f := func(i int) bool {
 		return list[i] == needle
 	}
 
-	return SliceIndex(len(list), f) > -1
+	return SliceIndex(len(list), f)
+}
+
+// IsStringInSlice looks for a string inside a slice and return true if it exists
+func IsStringInSlice(list []string, needle string) bool {
+	return GetStringIndexInSlice(list, needle) > -1
 }
--- search_test.go
+++ search_test.go
@@ -2,10 +2,26 @@ package gostrutils
 
 import "testing"
 
+func TestGetStringIndexInSliceFound(t *testing.T) {
+	list := genList()
+	idx := GetStringIndexInSlice(list, "hello")
+	if idx == -1 {
+		t.Error("Unable to find 'hello' in slice")
+	}
+}
+
+func TestGetStringIndexInSliceNotFound(t *testing.T) {
+	list := genList()
+	idx := GetStringIndexInSlice(list, "planet")
+	if idx != -1 {
+		t.Error("Unable to find 'hello' on the slice")
+	}
+}
+
 func TestSearchFound(t *testing.T) {
 	list := genList()
 
-	found := SearchStringInSlice(list, "hello")
+	found := IsStringInSlice(list, "hello")
 	if !found {
 		t.Error("Unable to find 'hello' on the slice")
 	}
@@ -14,7 +30,7 @@ func TestSearchFound(t *testing.T) {
 func TestSearchNotFound(t *testing.T) {
 	list := genList()
 
-	found := SearchStringInSlice(list, "planet")
+	found := IsStringInSlice(list, "planet")
 	if found {
 		t.Error("Able to find 'planet' at the list")
 	}
