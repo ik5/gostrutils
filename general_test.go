cb642a04ef273e246bb2ca89e93cbe8ef62661f7
Ido Kanner <idok@linesip.com>
2018-10-31 18:37:00 +0200

    fixed test names

--- general_test.go
+++ general_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_ValidDetectMimeTypeFromContent(t *testing.T) {
+func TestValidDetectMimeTypeFromContent(t *testing.T) {
 	hello := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
 	mime := DetectMimeTypeFromContent(hello)
 	if !strings.HasPrefix(mime, "text/plain") {
@@ -13,7 +13,7 @@ func Test_ValidDetectMimeTypeFromContent(t *testing.T) {
 	}
 }
 
-func Test_InvalidDetectMimeTypeFromContent(t *testing.T) {
+func TestInvalidDetectMimeTypeFromContent(t *testing.T) {
 	htmlHead := []byte{0x3C, 0x68, 0x65, 0x61, 0x64, 0x3e}
 	mime := DetectMimeTypeFromContent(htmlHead)
 	if strings.HasPrefix(mime, "text/plain") {
@@ -21,7 +21,7 @@ func Test_InvalidDetectMimeTypeFromContent(t *testing.T) {
 	}
 }
 
-func Test_StrToPointer(t *testing.T) {
+func TestStrToPointer(t *testing.T) {
 	str := "hello"
 	pStr := StrToPointer(str)
 	if pStr == nil {
