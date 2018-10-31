961265b2f796b74a154f921350178e3038e601c4
Ido Kanner <idok@linesip.com>
2018-10-31 17:25:48 +0200

    adding more documentation

--- doc.go
+++ doc.go
@@ -9,5 +9,10 @@ to.
 Basics
 
 The basic logic of Go, is to use bytes as a non numeric holder.
+The basics of the following package is to gain the ability to hold more support for string based on functions that are missing.
+
+The project itself built with files that holds the subject of what they are doing.
+
+The helpers.go file, holds functions that are not string related functions, but help creating that string support.
 */
 package gostrutils
