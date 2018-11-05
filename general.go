a36c706b080819c0815a6d39a5bfad60c4c4f6db
Ido Kanner <idok@linesip.com>
2018-11-05 19:46:03 +0200

    typos

--- general.go
+++ general.go
@@ -2,14 +2,14 @@ package gostrutils
 
 import "net/http"
 
-// DetectMimeTypeFromContent takes a slice of bytes, and try to detect the content type thst is in use
+// DetectMimeTypeFromContent takes a slice of bytes, and try to detect the content type that is in use
 func DetectMimeTypeFromContent(content []byte) (contentType string) {
 	contentType = http.DetectContentType(content)
 	return
 }
 
 // StrToPointer returns the address of string.
-// It is good use when you have  a  literal string thst requiers to be converted to pointer
+// It is good use when you have a literal string that requires to be converted to pointer
 func StrToPointer(str string) *string {
 	return &str
 }
