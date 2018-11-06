2343f490154d503fc5acdc554e034c4257d9f616
Ido Kanner <ikanner@redhat.com>
2018-11-06 10:11:33 +0200

    adding support for travis

--- encoding_gsm0338_test.go
+++ encoding_gsm0338_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestUTF8ToGsm0338(t *testing.T) {
-	s := "me@example.com"
+	s := "Hello World"
 	gsm := UTF8ToGsm0338(s)
 	utf8 := GSM0338ToUTF8(gsm)
 	fmt.Printf("word before: %s\nword after gsm: %s\nword after utf8: %s\n", s, gsm, utf8)
