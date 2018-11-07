a5ca73fe7030a7c496e1f18bfb7de7daae042499
Ido Kanner <ikanner@redhat.com>
2018-11-07 09:08:22 +0200

    added testing for EncodeUTF16 big and little endian

--- encoding_unicode_test.go
+++ encoding_unicode_test.go
@@ -2,9 +2,39 @@ package gostrutils
 
 import (
 	"testing"
+	"reflect"
 )
+
+func TestEncodeUTF16BigEndian(t *testing.T) {
+	b := utf16ByteSliceBigEndian()
+	result := EncodeUTF16("TEST㑬", true, true)
+	if !reflect.DeepEqual(b, result) {
+		t.Errorf("Result and expected result are different:\n%+v\n%+v", b, result)
+	}
+}
+
+func TestEncodeUTF16LittleEndian(t *testing.T) {
+	b := utf16ByteSliceLittleEndian()
+	result := EncodeUTF16("TEST㑬", false, true)
+	if !reflect.DeepEqual(b, result) {
+		t.Errorf("Result and expected result are different:\n%+v\n%+v", b, result)
+	}
+}
+
 func TestDecodeUTF16(t *testing.T) {
-	b := []byte{
+	b := utf16ByteSliceLittleEndian()
+	utf8, err := DecodeUTF16(b)
+	if err != nil {
+		t.Errorf("Error decoding: %s", err)
+	}
+
+	if utf8 != "TEST㑬" {
+		t.Errorf("Expected 'TEST㑬', got: '%s'", utf8)
+	}
+}
+
+func utf16ByteSliceLittleEndian() []byte {
+	return []byte{
 		0xff, // BOM
 		0xfe, // BOM
 		'T',
@@ -18,13 +48,22 @@ func TestDecodeUTF16(t *testing.T) {
 		0x6C,
 		0x34,
 	}
+}
 
-	utf8, err := DecodeUTF16(b)
-	if err != nil {
-		t.Errorf("Error decoding: %s", err)
+func utf16ByteSliceBigEndian() []byte {
+	return []byte{
+		0xfe, // BOM
+		0xff, // BOM
+		0x00,
+		'T',
+		0x00,
+		'E',
+		0x00,
+		'S',
+		0x00,
+		'T',
+		0x34,
+		0x6C,
 	}
 
-	if utf8 != "TEST㑬" {
-		t.Errorf("Expected 'TEST㑬', got: '%s'", utf8)
-	}
 }
