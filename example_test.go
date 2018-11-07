3c0d4bcf93197bbba26cecd2e0f440fd0c05479f
Ido Kanner <ikanner@redhat.com>
2018-11-07 11:34:38 +0200

    example for UTF8ToGsm0338

--- example_test.go
+++ example_test.go
@@ -25,3 +25,11 @@ func ExamplePointerToStr() {
 	newHello := gostrutils.PointerToStr(pHello)
 	fmt.Printf("empty: %s, pHello: %s\n", empty, newHello)
 }
+
+func ExampleUTF8ToGsm0338() {
+	msg := gostrutils.UTF8ToGsm0338("Please email me to user@example.com")
+	// send the SMS message here
+	if !sendMessage(msg) {
+		panic("Cannot send a message")
+	}
+}
