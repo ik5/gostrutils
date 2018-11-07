6c0b67f10d1258731125b465de2846458aab785a
Ido Kanner <ikanner@redhat.com>
2018-11-07 08:31:01 +0200

    testing decode of utf16

--- encoding_unicode_test.go
+++ encoding_unicode_test.go
@@ -1,2 +1,30 @@
 package gostrutils
 
+import (
+	"testing"
+)
+func TestDecodeUTF16(t *testing.T) {
+	b := []byte{
+		0xff, // BOM
+		0xfe, // BOM
+		'T',
+		0x00,
+		'E',
+		0x00,
+		'S',
+		0x00,
+		'T',
+		0x00,
+		0x6C,
+		0x34,
+	}
+
+	utf8, err := DecodeUTF16(b)
+	if err != nil {
+		t.Errorf("Error decoding: %s", err)
+	}
+
+	if utf8 != "TEST㑬" {
+		t.Errorf("Expected 'TEST㑬', got: '%s'", utf8)
+	}
+}
