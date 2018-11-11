3d1bdae6985bd4e9d3ac811a0cf43adf831ef9a2
Ido Kanner <ikanner@redhat.com>
2018-11-11 15:12:54 +0200

    Added test for ToFloat32 and ToFloat64

--- numbers_test.go
+++ numbers_test.go
@@ -117,6 +117,20 @@ func TestToFloat32DefaultWithDefault(t *testing.T) {
 	}
 }
 
+func TestToFloat32Valid(t *testing.T) {
+	num := ToFloat32("3.14")
+	if num != 3.14 {
+		t.Errorf("Expected '3.14', got '%f'", num)
+	}
+}
+
+func TestToFloat32Zero(t *testing.T) {
+	num := ToFloat32("Hello World")
+	if num != 0.0 {
+		t.Errorf("Expected '0.0', got '%f'", num)
+	}
+}
+
 func TestToFloat64DefaultSimple(t *testing.T) {
 	num := ToFloat6Default("3.14", 1.0)
 	if num != 3.14 {
@@ -128,5 +142,19 @@ func TestToFloat64DefaultWithDefault(t *testing.T) {
 	num := ToFloat6Default(".", 3.14)
 	if num != 3.14 {
 		t.Errorf("Expected '3.14', got %f", num)
+	}
+}
+
+func TestToFloat64Valid(t *testing.T) {
+	num := ToFloat64("3.14")
+	if num != 3.14 {
+		t.Errorf("Expected '3.14', got '%f'", num)
+	}
+}
+
+func TestToFloat64Zero(t *testing.T) {
+	num := ToFloat64("Hello World")
+	if num != 0.0 {
+		t.Errorf("Expected '0.0', got '%f'", num)
 	}
 }
