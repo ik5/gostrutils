518875692433389074e942a505785c5efc01a93e
Ido Kanner <idok@linesip.com>
2019-09-24 13:28:49 +0300

    Added new function HexToUTF16Runes

--- encoding_unicode.go
+++ encoding_unicode.go
@@ -8,6 +8,13 @@ import (
 	"unicode/utf8"
 )
 
+// BOM Types
+const (
+	BOMNone = iota
+	BOMBE
+	BOMLE
+)
+
 // EncodeUTF16 get a utf8 string and translate it into a slice of bytes
 func EncodeUTF16(s string, bigEndian, addBom bool) []byte {
 	r := []rune(s)
@@ -51,19 +58,22 @@ func DecodeUTF16(b []byte) (string, error) {
 
 	// if there is BOM, we start at 2, otherwise at the beginning
 	start := 2
-	if bom == 0 {
+	if bom == BOMNone {
 		start = 0
 	}
 	for i := start; i < lb; i += 2 {
 		//assuming bom is big endian if 0 returned
-		if bom == 0 || bom == 1 {
+		if bom == BOMNone || bom == BOMBE {
 			u16s[0] = uint16(b[i+1]) + (uint16(b[i]) << 8)
-		} else if bom == 2 {
+		} else if bom == BOMLE {
 			u16s[0] = uint16(b[i]) + (uint16(b[i+1]) << 8)
 		}
 		r := utf16.Decode(u16s)
 		n := utf8.EncodeRune(b8buf, r[0])
-		ret.Write([]byte(string(b8buf[:n])))
+		_, err := ret.Write([]byte(string(b8buf[:n])))
+		if err != nil {
+			return "", err
+		}
 	}
 
 	return ret.String(), nil
@@ -77,12 +87,65 @@ func UTF16Bom(b []byte) int8 {
 	}
 
 	if b[0] == 0xFE && b[1] == 0xFF {
-		return 1
+		return BOMBE
 	}
 
 	if b[0] == 0xFF && b[1] == 0xFE {
-		return 2
+		return BOMLE
 	}
 
-	return 0
+	return BOMNone
+}
+
+// HexToUTF16Runes takes a hex based string and converts it into UTF16 runes
+//
+// Such string looks like:
+//
+//    "\x00H\x00e\x00o\x00 \x00W\x00o\x00r\x00l\x00d"
+//    "\x05\xe9\x05\xdc\x05\xd5\x05\xdd\x00 \x05\xe2\x05\xd5\x05\xdc\x05\xdd"
+//
+func HexToUTF16Runes(s string, bigEndian bool) []rune {
+	var chars []byte
+	var position int
+	length := len(s)
+
+	// extract bytes from string
+	for {
+		if position >= length {
+			break
+		}
+		if s[position] == '\\' {
+			position++
+			chars = append(chars, s[position+1], s[position+2])
+			position += 2
+			continue
+		}
+
+		chars = append(chars, s[position])
+		position++
+		continue
+	}
+
+	var runes []rune
+	position = 0
+	length = len(chars)
+
+	// convert bytes two runes
+	for {
+		if position >= length {
+			break
+		}
+		aByte := []byte{chars[position], chars[position+1]}
+		var aRune uint16
+		if bigEndian {
+			aRune = binary.BigEndian.Uint16(aByte)
+		} else {
+			aRune = binary.LittleEndian.Uint16(aByte)
+		}
+		unicodeChar := utf16.Decode([]uint16{aRune})
+		runes = append(runes, unicodeChar...)
+		position += 2
+	}
+
+	return runes
 }
